f0d5b7e5eef01e8195331df1bdd81c91235d0b73
Jonas <frankemoelle.jonas@gmail.com>
2022-05-29 16:21:55 +0200

    testing q2

--- layer3_consumer.py
+++ layer3_consumer.py
@@ -53,7 +53,7 @@ start = time.time()
 
 while True:
     # msg_q1 = consumer_q1.receive()
-    # msg_q2 = consumer_q2.receive()
+    msg_q2 = consumer_q2.receive()
     msg_q3 = consumer_q3.receive()
     # msg_q4 = consumer_q4.receive()
     
@@ -72,30 +72,33 @@ while True:
     #         consumer_q1.acknowledge(msg_q1)
     #     except:
     #         consumer_q1.negative_acknowledge(msg_q1)       
-    # if msg_q2:
-    #     try:
-    #         data_q2 = msg_q2.data()
-            
-    #         data_json = json.loads(data_q2)
-    #         project_name = list(data_json.keys())[0]
-    #         num_commits = data_json[project_name]  
-    #         store_q2(project_name, num_commits)
-            
-    #         consumer_q2.acknowledge(msg_q2)
-    #     except:
-    #         consumer_q2.negative_acknowledge(msg_q2)
-            
-    if msg_q3:
+    if msg_q2:
         try:
-            data_q3 = msg_q3.data()
-            store_q3(data_q3)
+            data_q2 = msg_q2.data()
+            
+            data_json = json.loads(data_q2)
+            project_name = list(data_json.keys())[0]
+            num_commits = data_json[project_name]  
+            store_q2(project_name, num_commits)
             
             end = time.time()
             print('curr time: ', end-start)
     
-            consumer_q3.acknowledge(msg_q3)
+            consumer_q2.acknowledge(msg_q2)
         except:
-            consumer_q3.negative_acknowledge(msg_q3)
+            consumer_q2.negative_acknowledge(msg_q2)
+            
+    # if msg_q3:
+    #     try:
+    #         data_q3 = msg_q3.data()
+    #         store_q3(data_q3)
+            
+    #         end = time.time()
+    #         print('curr time: ', end-start)
+    
+    #         consumer_q3.acknowledge(msg_q3)
+    #     except:
+    #         consumer_q3.negative_acknowledge(msg_q3)
             
     # if msg_q4:
     #     try:
