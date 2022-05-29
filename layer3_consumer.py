5ceb94bce3dffb823d812bd1508cc4cf5cc4c17e
Jonas <frankemoelle.jonas@gmail.com>
2022-05-29 16:31:22 +0200

    checking q134

--- layer3_consumer.py
+++ layer3_consumer.py
@@ -52,58 +52,61 @@ def store_q4(data):
 start = time.time()
 
 while True:
-    # msg_q1 = consumer_q1.receive()
-    msg_q2 = consumer_q2.receive()
+    msg_q1 = consumer_q1.receive()
+    #msg_q2 = consumer_q2.receive()
     msg_q3 = consumer_q3.receive()
-    # msg_q4 = consumer_q4.receive()
+    msg_q4 = consumer_q4.receive()
     
-    # if msg_q1:
-    #     try:
-    #         data_q1 = msg_q1.data()
-    #         store_q1(data_q1)
-            
-    #         print('current RESULTS_Q1: ')
-    #         for key, val in RESULTS_Q1.items():
-    #             print(key, val)
-                
-    #         end = time.time()
-    #         print('curr time: ', end-start)
-            
-    #         consumer_q1.acknowledge(msg_q1)
-    #     except:
-    #         consumer_q1.negative_acknowledge(msg_q1)       
-    if msg_q2:
+    if msg_q1:
+        print('received msg Q1')
         try:
-            data_q2 = msg_q2.data()
+            data_q1 = msg_q1.data()
+            store_q1(data_q1)
             
-            data_json = json.loads(data_q2)
-            project_name = list(data_json.keys())[0]
-            num_commits = data_json[project_name]  
-            store_q2(project_name, num_commits)
-            
+            # print('current RESULTS_Q1: ')
+            # for key, val in RESULTS_Q1.items():
+            #     print(key, val)
+                
             end = time.time()
             print('curr time: ', end-start)
-    
-            consumer_q2.acknowledge(msg_q2)
-        except:
-            consumer_q2.negative_acknowledge(msg_q2)
             
-    # if msg_q3:
+            consumer_q1.acknowledge(msg_q1)
+        except:
+            consumer_q1.negative_acknowledge(msg_q1)       
+    # if msg_q2:
     #     try:
-    #         data_q3 = msg_q3.data()
-    #         store_q3(data_q3)
+    #         data_q2 = msg_q2.data()
+            
+    #         data_json = json.loads(data_q2)
+    #         project_name = list(data_json.keys())[0]
+    #         num_commits = data_json[project_name]  
+    #         store_q2(project_name, num_commits)
             
     #         end = time.time()
     #         print('curr time: ', end-start)
     
-    #         consumer_q3.acknowledge(msg_q3)
+    #         consumer_q2.acknowledge(msg_q2)
     #     except:
-    #         consumer_q3.negative_acknowledge(msg_q3)
+    #         consumer_q2.negative_acknowledge(msg_q2)
             
-    # if msg_q4:
-    #     try:
-    #         data_q4 = msg_q4.data()
-    #         store_q4(data_q4)
-    #         consumer_q4.acknowledge(msg_q4)
-    #     except:
-    #         consumer_q4.negative_acknowledge(msg_q4)
+    if msg_q3:
+        print('received msg Q3')
+        try:
+            data_q3 = msg_q3.data()
+            store_q3(data_q3)
+            
+            # end = time.time()
+            # print('curr time: ', end-start)
+    
+            consumer_q3.acknowledge(msg_q3)
+        except:
+            consumer_q3.negative_acknowledge(msg_q3)
+            
+    if msg_q4:
+        print('received msg Q4')
+        try:
+            data_q4 = msg_q4.data()
+            store_q4(data_q4)
+            consumer_q4.acknowledge(msg_q4)
+        except:
+            consumer_q4.negative_acknowledge(msg_q4)
