818d252f502099c41b35fe0a0d37b3819739bf05
Jonas <frankemoelle.jonas@gmail.com>
2022-05-29 19:29:29 +0200

    testing consumer

--- layer3_consumer.py
+++ layer3_consumer.py
@@ -54,11 +54,10 @@ count=0
 start = time.time()
 while True:
     msg_q1 = consumer_q1.receive()
-    # msg_q2 = consumer_q2.receive()
-    # msg_q3 = consumer_q3.receive()
-    # msg_q4 = consumer_q4.receive()
+    msg_q2 = consumer_q2.receive()
+    msg_q3 = consumer_q3.receive()
+    msg_q4 = consumer_q4.receive()
     
-    #if msg_q1:
     try:
         data_q1 = msg_q1.data()
         store_q1(data_q1)
@@ -72,38 +71,42 @@ while True:
         consumer_q1.acknowledge(msg_q1)
     except:
         consumer_q1.negative_acknowledge(msg_q1)       
-    # if msg_q2:
-    #     count+=1
-    #     print('count: ', count)
-    #     try:
-    #         data_q2 = msg_q2.data()
-            
-    #         data_json = json.loads(data_q2)
-    #         project_name = list(data_json.keys())[0]
-    #         num_commits = data_json[project_name]  
-    #         store_q2(project_name, num_commits)
-            
-    #         consumer_q2.acknowledge(msg_q2)
-    #     except:
-    #         consumer_q2.negative_acknowledge(msg_q2)
-            
-    # if msg_q3:
-    #     count+=1
-    #     print('count: ', count)
-    #     try:
-    #         data_q3 = msg_q3.data()
-    #         store_q3(data_q3)
-    
-    #         consumer_q3.acknowledge(msg_q3)
-    #     except:
-    #         consumer_q3.negative_acknowledge(msg_q3)
-            
-    # if msg_q4:
-    #     count+=1
-    #     print('count: ', count)
-    #     try:
-    #         data_q4 = msg_q4.data()
-    #         store_q4(data_q4)
-    #         consumer_q4.acknowledge(msg_q4)
-    #     except:
-    #         consumer_q4.negative_acknowledge(msg_q4)
+   
+    try:
+        data_q2 = msg_q2.data()
+        
+        data_json = json.loads(data_q2)
+        project_name = list(data_json.keys())[0]
+        num_commits = data_json[project_name]  
+        store_q2(project_name, num_commits)
+        
+        count+=1
+        print('count: ', count)
+        
+        consumer_q2.acknowledge(msg_q2)
+    except:
+        consumer_q2.negative_acknowledge(msg_q2)
+        
+
+    try:
+        data_q3 = msg_q3.data()
+        store_q3(data_q3)
+        
+        count+=1
+        print('count: ', count)
+
+        consumer_q3.acknowledge(msg_q3)
+    except:
+        consumer_q3.negative_acknowledge(msg_q3)
+        
+
+    try:
+        data_q4 = msg_q4.data()
+        store_q4(data_q4)
+        
+        count+=1
+        print('count: ', count)
+        
+        consumer_q4.acknowledge(msg_q4)
+    except:
+        consumer_q4.negative_acknowledge(msg_q4)
