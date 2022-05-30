702b2393809d209338ca91eb52f4a4e1e0a55464
Jonas <frankemoelle.jonas@gmail.com>
2022-05-30 18:39:08 +0200

    updated consumers

--- layer3_consumer2.py
+++ layer3_consumer2.py
@@ -7,7 +7,7 @@ import time
 client = pulsar.Client('pulsar://localhost:6650')
 
 # Subscribe to a topic and subscription
-consumer_q2 = client.subscribe('topic_q134_2', subscription_name='github_sub_1', consumer_type=pulsar.ConsumerType.Shared)
+consumer_q134 = client.subscribe('topic_q134_2', subscription_name='github_sub_1', consumer_type=pulsar.ConsumerType.Shared)
 
 
 RESULTS_Q2 = {}   
@@ -21,21 +21,26 @@ def store_q2(project_name, num_commits):
 count=0
 start = time.time()
 while True:
-    msg_q2 = consumer_q2.receive()
+    msg_q2 = consumer_q134.receive()
 
    
     try:
-        data_q2 = msg_q2.data()
+        data_q134 = msg_q2.data()
         
-        data_json = json.loads(data_q2)
-        project_name = list(data_json.keys())[0]
-        num_commits = data_json[project_name]  
-        store_q2(project_name, num_commits)
+        data_json = json.loads(data_q134)
+        
+        project_name = data_json['project_name']
+        language = data_json['language']
+        has_tests = data_json['has_tests'] # returns boolean
+        has_cont_int = data_json['has_cont_int'] # returns boolean
+        
+        # TODO: store  in database
+        # store_q2(project_name, num_commits)
         
         count+=1
         print('count: ', count)
         
-        consumer_q2.acknowledge(msg_q2)
+        consumer_q134.acknowledge(msg_q2)
     except:
-        consumer_q2.negative_acknowledge(msg_q2)
+        consumer_q134.negative_acknowledge(msg_q2)
     
